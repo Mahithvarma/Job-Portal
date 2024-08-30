6ccdf5307b88b1cfddf31b283178933784ceb567
JAMPANA MAHEETH SAI DURGA VARMA <121691092+Mahithvarma@users.noreply.github.com>
2024-08-31 00:19:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: ["http://jobhunt-mahith.netlify.app", "https://jobhunt-mahith.netlify.app"],
     credentials: true
 }));
 
